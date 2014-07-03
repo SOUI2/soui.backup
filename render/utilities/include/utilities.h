e96bc1a26f08bdc60587eabd1a6115b697729613
setoutsoft@qq.com <setoutsoft@qq.com@5312f6f6-3d4f-41ab-b741-a9c9e676edf4>
2014-07-03 02:30:33 +0000

    remove render branch

--- render/utilities/include/utilities.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "utilities-def.h"
-
-namespace SOUI
-{
-    void UTILITIES_API DuiHiMetricToPixel(const SIZEL * lpSizeInHiMetric, LPSIZEL lpSizeInPix);
-    void UTILITIES_API DuiPixelToHiMetric(const SIZEL * lpSizeInPix, LPSIZEL lpSizeInHiMetric);
-}//end of namespace SOUI
