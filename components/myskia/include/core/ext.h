d77fcf4524e3a1e52211702198ea705777399b81
setoutsoft <setoutsoft@users.noreply.github.com>
2014-07-11 15:38:05 +0000

    move skia and freetype to third-part

--- components/myskia/include/core/ext.h
+++
@@ -1,6 +0,0 @@
-
-#include "SkBitmap.h"
-#include "SkCanvas.h"
-extern char* fontName[];
-SK_API void* operator new( size_t nSize,int nLine);
-SK_API const SkBitmap& Canvas2Bitmap(SkCanvas* canvas);
