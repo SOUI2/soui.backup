62172a1aa2af6f9fe6ca30311338d77a84233ec6
setoutsoft@qq.com <setoutsoft@qq.com@5312f6f6-3d4f-41ab-b741-a9c9e676edf4>
2014-07-31 03:46:54 +0000

    改写messsagebox模板。

--- trunk/SOUI/src/controls/SMessageBox.cpp
+++ trunk/SOUI/src/controls/SMessageBox.cpp
@@ -20,134 +20,29 @@ namespace SOUI
         return TRUE;
     }
 
+    SMessageBoxImpl::SMessageBoxImpl() :SHostDialog(NULL)
+    {
+
+    }
+    
+    static struct MsgBoxInfo
+    {
+        LPCTSTR pszText;
+        LPCTSTR pszCaption;
+        UINT    uType;
+    }s_MsgBoxInfo;
+    
     INT_PTR SMessageBoxImpl::MessageBox( HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType )
     {
         if(!s_xmlMsgTemplate) return ::MessageBox(hWnd,lpText,lpCaption,uType);
-        
-        if(!hWnd) hWnd=GetActiveWindow();
-        Create(hWnd,WS_POPUPWINDOW,0,0,0,10,10);
-        pugi::xml_node uiRoot=s_xmlMsgTemplate.child(L"SOUI");
-        InitFromXml(uiRoot);
-
-
-        switch(uType&0x0F)
-        {
-        case MB_ABORTRETRYIGNORE:
-            {
-                FindChildByName(NAME_MSGBOX_BTN1PANEL)->SetVisible(FALSE);
-                FindChildByName(NAME_MSGBOX_BTN2PANEL)->SetVisible(FALSE);
-                SWindow *pBtnPanel=FindChildByName(NAME_MSGBOX_BTN3PANEL);
-                pBtnPanel->SetVisible(TRUE);
-                SWindow *pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN1);
-                pBtn->SetWindowText(_T("中止"));pBtn->SetID(IDABORT);
-                pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN2);
-                pBtn->SetWindowText(_T("重试"));pBtn->SetID(IDRETRY);
-                pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN3);
-                pBtn->SetWindowText(_T("忽略"));pBtn->SetID(IDIGNORE);
-            }
-            break;
-        case MB_YESNOCANCEL:
-            {
-                FindChildByName(NAME_MSGBOX_BTN1PANEL)->SetVisible(FALSE);
-                FindChildByName(NAME_MSGBOX_BTN2PANEL)->SetVisible(FALSE);
-                SWindow *pBtnPanel=FindChildByName(NAME_MSGBOX_BTN3PANEL);
-                pBtnPanel->SetVisible(TRUE);
-                SWindow *pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN1);
-                pBtn->SetWindowText(_T("是"));pBtn->SetID(IDYES);
-                pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN2);
-                pBtn->SetWindowText(_T("否"));pBtn->SetID(IDNO);
-                pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN3);
-                pBtn->SetWindowText(_T("取消"));pBtn->SetID(IDCANCEL);
-            }
-            break;
-        case MB_OKCANCEL:
-            {
-                FindChildByName(NAME_MSGBOX_BTN1PANEL)->SetVisible(FALSE);
-                FindChildByName(NAME_MSGBOX_BTN3PANEL)->SetVisible(FALSE);
-
-                SWindow *pBtnPanel=FindChildByName(NAME_MSGBOX_BTN2PANEL);
-                pBtnPanel->SetVisible(TRUE);
-
-                SWindow *pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN1);
-                pBtn->SetWindowText(_T("确定"));    pBtn->SetID(IDOK);
-                pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN2);
-                pBtn->SetWindowText(_T("取消"));    pBtn->SetID(IDCANCEL);
-            }
-            break;
-        case MB_YESNO:
-            {
-                FindChildByName(NAME_MSGBOX_BTN1PANEL)->SetVisible(FALSE);
-                FindChildByName(NAME_MSGBOX_BTN3PANEL)->SetVisible(FALSE);
-
-                SWindow *pBtnPanel=FindChildByName(NAME_MSGBOX_BTN2PANEL);
-                pBtnPanel->SetVisible(TRUE);
-
-                SWindow *pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN1);
-                pBtn->SetWindowText(_T("是"));    pBtn->SetID(IDYES);
-                pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN2);
-                pBtn->SetWindowText(_T("否"));    pBtn->SetID(IDNO);
-            }
-            break;
-        case MB_RETRYCANCEL:
-            {
-                FindChildByName(NAME_MSGBOX_BTN1PANEL)->SetVisible(FALSE);
-                FindChildByName(NAME_MSGBOX_BTN3PANEL)->SetVisible(FALSE);
-
-                SWindow *pBtnPanel=FindChildByName(NAME_MSGBOX_BTN2PANEL);
-                pBtnPanel->SetVisible(TRUE);
-
-                SWindow *pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN1);
-                pBtn->SetWindowText(_T("重试"));    pBtn->SetID(IDRETRY);
-                pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN2);
-                pBtn->SetWindowText(_T("取消"));    pBtn->SetID(IDCANCEL);
-            }
-            break;
-        case MB_OK:
-            {
-                FindChildByName(NAME_MSGBOX_BTN2PANEL)->SetVisible(FALSE);
-                FindChildByName(NAME_MSGBOX_BTN3PANEL)->SetVisible(FALSE);
-
-                SWindow *pBtnPanel=FindChildByName(NAME_MSGBOX_BTN1PANEL);
-                pBtnPanel->SetVisible(TRUE);
-                SWindow *pBtn=pBtnPanel->FindChildByName(NAME_MSGBOX_BTN1);
-                pBtn->SetWindowText(_T("确定"));    pBtn->SetID(IDOK);
-            }
-            break;
-        default:
-            ASSERT(FALSE);
-            break;
-        }
-        const wchar_t *pszFrameAttr=uiRoot.attribute(L"frame_size").value();
-        CRect rcFrame;
-        swscanf(pszFrameAttr,L"%d,%d,%d,%d",&rcFrame.left,&rcFrame.top,&rcFrame.right,&rcFrame.bottom);
-        CSize szMin;
-        const wchar_t *pszMinAttr=uiRoot.attribute(L"minsize").value();
-        swscanf(pszMinAttr,L"%d,%d",&szMin.cx,&szMin.cy);
-
-        SWindow * pTitle= FindChildByName(NAME_MSGBOX_TITLE);
-        ASSERT(pTitle);
-        pTitle->SetWindowText(lpCaption?lpCaption:_T("提示"));
-
-        SWindow * pMsg= FindChildByName(NAME_MSGBOX_TEXT);
-        ASSERT(pMsg);
-        pMsg->SetWindowText(lpText);
-
-        SetIcon(uType);
-
-        CRect rcText;
-        pMsg->GetWindowRect(&rcText);
-
-        CSize szWnd;
-        szWnd.cx=max(szMin.cx,rcText.Width()+rcFrame.left+rcFrame.right);
-        szWnd.cy=max(szMin.cy,rcText.Height()+rcFrame.top+rcFrame.bottom);
-
-        SetWindowPos(0,0,0,szWnd.cx,szWnd.cy,SWP_NOMOVE);
-        CenterWindow(m_hWnd);
+        s_MsgBoxInfo.pszText=lpText;
+        s_MsgBoxInfo.pszCaption=lpCaption;
+        s_MsgBoxInfo.uType=uType;
         
         return DoModal(hWnd);
     }
 
-    BOOL SMessageBoxImpl::SetIcon( UINT uType )
+    BOOL SMessageBoxImpl::OnSetIcon( UINT uType )
     {
         SIconWnd *pIcon=(SIconWnd *)FindChildByName(NAME_MSGBOX_ICON);
         if(!pIcon) return FALSE;
@@ -169,13 +64,131 @@ namespace SOUI
         return TRUE;
     }
 
+    struct MSGBTN_TEXT
+    {
+        int   nBtns;
+        struct
+        {
+            UINT uBtnID;
+            TCHAR szText[20];       
+        }btnInfo[3];
+    }g_msgBtnText[]=
+    {
+        //MB_OK
+        {
+            1,
+            {
+                {IDOK,    _T("ok")},
+                {0,    _T("")},
+                {0,   _T("")}
+            }
+        },
+        //MB_OKCANCEL
+        {
+            2,
+            {
+                {IDOK,    _T("ok")},
+                {IDCANCEL,    _T("cancel")},
+                {0,    _T("")}
+            }
+        },
+        //MB_ABORTRETRYIGNORE
+        {
+            3,
+            {
+                {IDABORT,_T("abort")},
+                {IDRETRY,_T("retry")},
+                {IDIGNORE,_T("ignore")}
+            }
+        },
+        //MB_YESNOCANCEL
+        {
+            3,
+            {
+                {IDYES,_T("yes")},
+                {IDNO,_T("no")},
+                {IDCANCEL,_T("cancel")}
+            }
+        },
+        //MB_YESNO
+        {
+            2,
+            {
+                {IDYES,_T("yes")},
+                {IDNO,_T("no")},
+                {0,_T("")}
+            }
+        },
+        //MB_RETRYCANCEL
+        {
+            2,
+            {
+                {IDRETRY,_T("retry")},
+                {IDCANCEL,_T("cancel")},
+                {0,_T("")}
+            }
+        }
+    };
+    
+    const WCHAR * g_wcsNameOfBtns[] =
+    {
+        NAME_MSGBOX_BTN1,
+        NAME_MSGBOX_BTN2,
+        NAME_MSGBOX_BTN3
+    };
+    
+    BOOL SMessageBoxImpl::OnInitDialog( HWND wnd, LPARAM lInitParam )
+    {
+        pugi::xml_node uiRoot=s_xmlMsgTemplate.child(L"SOUI");
+        
+        InitFromXml(uiRoot);
+        UINT uType = s_MsgBoxInfo.uType&0x0F;
+
+        STabCtrl *pBtnSwitch= FindChildByName2<STabCtrl>(NAME_MSGBOX_BTNSWITCH);
+        pBtnSwitch->SetCurSel(g_msgBtnText[uType].nBtns-1);
+        
+        SWindow *pBtnPanel=pBtnSwitch->GetItem(g_msgBtnText[uType].nBtns-1);
+        
+        for(int i=0; i<g_msgBtnText[uType].nBtns; i++)
+        {
+            SWindow *pBtn=pBtnPanel->FindChildByName(g_wcsNameOfBtns[i]);
+            pBtn->SetWindowText(TR(g_msgBtnText[uType].btnInfo[i].szText,_T("messagebox")));    
+            pBtn->SetID(g_msgBtnText[uType].btnInfo[i].uBtnID);
+        }
+        
+        const wchar_t *pszFrameAttr=uiRoot.attribute(L"frame_size").value();
+        CRect rcFrame;
+        swscanf(pszFrameAttr,L"%d,%d,%d,%d",&rcFrame.left,&rcFrame.top,&rcFrame.right,&rcFrame.bottom);
+        CSize szMin;
+        const wchar_t *pszMinAttr=uiRoot.attribute(L"minsize").value();
+        swscanf(pszMinAttr,L"%d,%d",&szMin.cx,&szMin.cy);
+
+        SWindow * pTitle= FindChildByName(NAME_MSGBOX_TITLE);
+        ASSERT(pTitle);
+        pTitle->SetWindowText(TR(s_MsgBoxInfo.pszCaption?s_MsgBoxInfo.pszCaption:_T("prompt"),_T("messagebox")));
+
+        SWindow * pMsg= FindChildByName(NAME_MSGBOX_TEXT);
+        ASSERT(pMsg);
+        pMsg->SetWindowText(TR(s_MsgBoxInfo.pszText,_T("messagebox")));
+
+        OnSetIcon(s_MsgBoxInfo.uType);
+
+        CRect rcText;
+        pMsg->GetWindowRect(&rcText);
+
+        CSize szWnd;
+        szWnd.cx=max(szMin.cx,rcText.Width()+rcFrame.left+rcFrame.right);
+        szWnd.cy=max(szMin.cy,rcText.Height()+rcFrame.top+rcFrame.bottom);
+
+        SetWindowPos(0,0,0,szWnd.cx,szWnd.cy,SWP_NOMOVE);     
+        CenterWindow(wnd);  
+        return 0;
+    }
 
     int SMessageBox( HWND hWnd, LPCTSTR lpText, LPCTSTR lpCaption, UINT uType )
     {
         SMessageBoxImpl duiMsgBox;
         return duiMsgBox.MessageBox(hWnd,lpText,lpCaption,uType);
     }
-
-
 
 }//end of namespace 
