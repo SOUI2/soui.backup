a5380cce583fd7f0ee8d6cfb87a45d27b91bee2f
setoutsoft@qq.com <setoutsoft@qq.com@5312f6f6-3d4f-41ab-b741-a9c9e676edf4>
2014-08-29 04:10:31 +0000

    增加由L.K修改的Caption控件，支持最大化拖动的操作系统检测。

--- trunk/SOUI/src/control/SCaption.cpp
+++ trunk/SOUI/src/control/SCaption.cpp
@@ -33,19 +33,12 @@ void SCaption::OnLButtonUp(UINT nFlags, CPoint point)
 void SCaption::OnMouseMove(UINT nFlags, CPoint point)
 {
 	HWND hHost=GetContainer()->GetHostHwnd();
-    if (WS_MAXIMIZE == (GetWindowLong(hHost,GWL_STYLE) & WS_MAXIMIZE) && m_bIsMaxDown)
+    if (WS_MAXIMIZE == (GetWindowLong(hHost,GWL_STYLE) & WS_MAXIMIZE) && m_bIsMaxDown && IsSuppotMaxMove())
 	{
-		::SendMessage(hHost,WM_SYSCOMMAND, SC_RESTORE | HTCAPTION,0);
-		
-		POINT pt;
-		::GetCursorPos(&pt);
+		::SendMessage(hHost,WM_SYSCOMMAND, SC_MOVE | HTCAPTION,0);
 
-		RECT rc;
-		::GetWindowRect(hHost, &rc);
-		::SetWindowPos(hHost, NULL, pt.x - (rc.right - rc.left)/2, pt.y - 5, rc.right - rc.left, rc.bottom - rc.top, 0);
-
-		::SendMessage(hHost, WM_SYSCOMMAND, SC_MOVE | HTCAPTION, 0);
 		m_bIsMaxDown = FALSE;
+
 		return;
 	}
 }
@@ -73,6 +66,37 @@ void SCaption::OnLButtonDblClk( UINT nFlags, CPoint point )
     }
 }
 
+BOOL SCaption::IsSuppotMaxMove()
+{
+	OSVERSIONINFOEX OSVerInfo;
+	BOOL bOsVersionInfoEx;
+
+	::ZeroMemory(&OSVerInfo,sizeof(OSVERSIONINFOEX));
+
+	// Get the OS Version Information
+	OSVerInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+	if(!(bOsVersionInfoEx = ::GetVersionEx((OSVERSIONINFO *)&OSVerInfo)))
+	{
+		OSVerInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+		if (!::GetVersionEx((OSVERSIONINFO *)&OSVerInfo)) 
+		{
+			return FALSE;
+		}
+	}
+	
+	/**
+	 * Does it support Windows 8.1,Windows 8,Windows 7,Windows Vista ?
+	 * I just ignore the Windows Server System
+	 * if you wanna get more information, you can click the following link:
+	 *			http://msdn.microsoft.com/en-us/library/ms724833.aspx
+	 */
+	if (OSVerInfo.wProductType == VER_NT_WORKSTATION
+		&& OSVerInfo.dwMajorVersion == 6)
+	{
+		return TRUE;
+	}
+	return FALSE;
+}
 
 }//namespace SOUI
 
