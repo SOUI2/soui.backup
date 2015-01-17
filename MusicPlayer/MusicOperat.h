3d5823964242617e0ecba46ff0ac64ed4b7a4cf0
setoutsoft <setoutsoft@qq.com>
2015-01-17 09:27:54 +0000

    整理播放逻辑，封装音频操作类，增加当显示前播放歌曲的状态 小可

--- MusicPlayer/MusicOperat.h
+++ MusicPlayer/MusicOperat.h
@@ -1,22 +1,47 @@
 /****************************************************************************
-*  功    能：拖拽获取音乐路径                                               *
+*  功    能：音频文件操作类                                                 *
 *  添 加 人：小可                                                           *
-*  添加时间：2014.01.17 00：27                                              *
+*  添加时间：2014.01.17 12：27                                              *
 *  版本类型：初始版本                                                       *
 *  联系方式：QQ-1035144170                                                  *
 ****************************************************************************/
+
+#pragma once
+#include "DropEx.h"
 #include "BassMusic.h"
 
-class CMusicOpreat 
+class CMusicOpreat:public IMusicState,public SHostWnd
 {
 public:
-	CMusicOpreat(void)
-	{
+	CMusicOpreat(void);
+	~CMusicOpreat(void);
 
-	};
-	~CMusicOpreat(void)
-	{
+	static CMusicOpreat *GetInstance();
 
-	};
+	//接口继承
+public:
+	//正在播放
+	virtual void OnPlaying(){}
+	//暂停状态
+	virtual void OnPause(){}
+	//停止播放
+	virtual void OnStop();
+	//加/慢 速播放
+	virtual void OnStalled(){}
 
+	//自定义
+public:
+	int                 nIndex;         //播放索引
+	HSTREAM             hStream;        //播放流
+	CMusicManagerMap	m_MusicManager;
+	CBassMusicEngine    *m_pBassMusic;
+	IMusicState			*m_pMainState;
+	
+	void InitDatas();
+	void OnButPrev();
+	void OnButPlay();
+	void OnButPause();
+	void OnButPlayNext();
+
+	void InsertMapInfo(int nNum, CString strPath, tagMusicInfo &pMuInfo);
 };
