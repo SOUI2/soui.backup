27c173e56ef95d199145b943d41728d8f2dd2b9c
LiJinggang <horatii@163.com>
2015-02-22 03:52:59 +0000

    调整脚本demo

--- demo/uires/lua/test.lua
+++ demo/uires/lua/test.lua
@@ -14,7 +14,7 @@ end
 function onEvtTstClick(args)
 	local txt3=SStringW(L"append",-1);
 	local sender=toSWindow(args.sender);
-	sender:GetParent():CreateChildrenFromString(L"<button pos=\"0,0,150,30\" class=\"normalbtn\" id=\"12321\" show=\"1\" on_command=\"onEvtTest2\">lua btn 中文</button>");
+	sender:GetParent():CreateChildrenFromString(L"<button pos=\"0,0,150,30\" on_command=\"onEvtTest2\">lua btn 中文</button>");
 	sender:SetVisible(0,1);
 	return 1;
 end
