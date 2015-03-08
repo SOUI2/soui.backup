930bd7b4895f4ab606876f49125a3d692b91d85a
setoutsoft <setoutsoft@5312f6f6-3d4f-41ab-b741-a9c9e676edf4>
2015-03-08 04:19:50 +0000

    调整代码行统计工具。

--- trunk/samples/SoTool/CodeLineCounterHandler.cpp
+++ trunk/samples/SoTool/CodeLineCounterHandler.cpp
@@ -1,7 +1,95 @@
 #include "StdAfx.h"
 #include "CodeLineCounterHandler.h"
 #include <helper/SplitString.h>
+#include "droptarget.h"
+//////////////////////////////////////////////////////////////////////////
+class CDropTarget_Dir : public CDropTarget
+{
+protected:
+    CCodeLineCounterHandler *m_pLineCounterHandler;
+public:
+    CDropTarget_Dir(CCodeLineCounterHandler *pLineCounterHandler):m_pLineCounterHandler(pLineCounterHandler)
+    {
+    }
+    ~CDropTarget_Dir()
+    {
+    }
+public:
+    virtual HRESULT STDMETHODCALLTYPE Drop( 
+        /* [unique][in] */ __RPC__in_opt IDataObject *pDataObj,
+        /* [in] */ DWORD grfKeyState,
+        /* [in] */ POINTL pt,
+        /* [out][in] */ __RPC__inout DWORD *pdwEffect)
+    {
+        FORMATETC format =
+        {
+            CF_HDROP, 0, DVASPECT_CONTENT, -1, TYMED_HGLOBAL
+        };
+        STGMEDIUM medium;
+        if(FAILED(pDataObj->GetData(&format, &medium)))
+        {
+            return S_FALSE;
+        }
 
+        HDROP hdrop = static_cast<HDROP>(GlobalLock(medium.hGlobal));
+
+        if(!hdrop)
+        {
+            return S_FALSE;
+        }
+
+        bool success = false;
+        TCHAR filename[MAX_PATH];
+        success=!!DragQueryFile(hdrop, 0, filename, MAX_PATH);
+        if(success) 
+        {
+            if(GetFileAttributes(filename) & FILE_ATTRIBUTE_DIRECTORY)
+            {
+                m_pLineCounterHandler->InitDir(filename);
+            }
+        }
+        DragFinish(hdrop);
+        GlobalUnlock(medium.hGlobal);
+
+
+        *pdwEffect=DROPEFFECT_LINK;
+        return S_OK;
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////
+// CLineInfo
+class CLineInfo
+{
+public:
+    CLineInfo(int _code=0,int _blank=0, int _remark=0)
+        :nCodeLines(_code)
+        ,nBlankLines(_blank)
+        ,nRemarkLines(_remark)
+    {
+    }
+
+    void operator = (const CLineInfo & src)
+    {
+        nCodeLines = src.nCodeLines;
+        nBlankLines = src.nBlankLines;
+        nRemarkLines = src.nRemarkLines;
+    }
+
+    CLineInfo & operator += (const CLineInfo & src)
+    {
+        nCodeLines += src.nCodeLines;
+        nBlankLines += src.nBlankLines;
+        nRemarkLines += src.nRemarkLines;
+        return *this;
+    }
+    int nCodeLines;
+    int nBlankLines;
+    int nRemarkLines;
+};
+
+//////////////////////////////////////////////////////////////////////////
+// CCodeLineCounterHandler
 CCodeLineCounterHandler::CCodeLineCounterHandler(void):m_pPageRoot(NULL)
 {
 }
@@ -14,11 +102,23 @@ void CCodeLineCounterHandler::OnInit( SWindow *pRoot )
 {
     m_pPageRoot = pRoot->FindChildByName(L"page_codeline");
     SASSERT(m_pPageRoot);
+
+    SWindow *pEditDir = m_pPageRoot->FindChildByName(L"edit_dir");
+    SASSERT(pEditDir);
+    IDropTarget *pDT = new CDropTarget_Dir(this);
+    pRoot->GetContainer()->RegisterDragDrop(pEditDir->GetSwnd(),pDT);
+
     m_pDirTree = m_pPageRoot->FindChildByName2<STreeCtrl>(L"tree_dir");
-    pugi::xml_document xmlCodeLinesCounterCfg;
-    LOADXML(xmlCodeLinesCounterCfg,L"codelinecounterconfig",L"xml");
+
+    pugi::xml_document xmlCodeSyntax;
+
+    SStringT strSyntax = SApplication::getSingleton().GetAppDir() + _T("\\syntax.xml");
+    if(!xmlCodeSyntax.load_file(strSyntax))
+    {
+        LOADXML(xmlCodeSyntax,L"syntax",L"xml");
+    }
     
-    pugi::xml_node xmlNode = xmlCodeLinesCounterCfg.child(L"config").child(L"filetypes");
+    pugi::xml_node xmlNode = xmlCodeSyntax.child(L"config").child(L"filetypes");
     if(xmlNode)
     {
         pugi::xml_node xmlType = xmlNode.child(L"filetype");
@@ -34,7 +134,7 @@ void CCodeLineCounterHandler::OnInit( SWindow *pRoot )
             xmlType = xmlType.next_sibling(L"filetype");
         }
     }
-    xmlNode = xmlCodeLinesCounterCfg.child(L"config").child(L"languages");
+    xmlNode = xmlCodeSyntax.child(L"config").child(L"languages");
     if(xmlNode)
     {
         SComboBox * pCbx = m_pPageRoot->FindChildByName2<SComboBox>(L"cbx_filetypes");
@@ -52,20 +152,26 @@ void CCodeLineCounterHandler::OnInit( SWindow *pRoot )
     }
 }
 
-void CCodeLineCounterHandler::OnBtnPickerDir()
-{
-
-}
 
 void CCodeLineCounterHandler::OnKillFocus_Dir( EventArgs *pEvt )
 {
     SEdit *pEdit = sobj_cast<SEdit>(pEvt->sender);
     SStringT strDir = pEdit->GetWindowText();
+    InitDir(strDir,TRUE);
+}
+
+
+void CCodeLineCounterHandler::InitDir( const SStringT & strDir,BOOL bInput/*=FALSE */)
+{
     DWORD attr = GetFileAttributes(strDir);
     if(attr==INVALID_FILE_ATTRIBUTES) return ;
     if((attr & FILE_ATTRIBUTE_DIRECTORY) == 0) return;
     if(strDir == m_strDir) return;
-
+    
+    if(!bInput)
+    {
+        m_pPageRoot->FindChildByName(L"edit_dir")->SetWindowText(strDir);
+    }
     m_strDir = strDir;
     m_pDirTree->RemoveAllItems();
     HSTREEITEM hRoot = m_pDirTree->InsertItem(_T("root"),0,1);
@@ -97,80 +203,6 @@ void CCodeLineCounterHandler::InitDirTree(HSTREEITEM hTreeItem,const SStringT & 
     }
 }
 
-void CCodeLineCounterHandler::OnBtnGo()
-{
-    if(m_strDir.IsEmpty())
-    {
-        SMessageBox(m_pPageRoot->GetContainer()->GetHostHwnd(),_T("没有指定扫描目录"),_T("错误"),MB_OK|MB_ICONSTOP);
-        return;
-    }
-    SStringT strTypes = m_pPageRoot->FindChildByName(L"cbx_filetypes")->GetWindowText();
-    SStringTList strlst;
-    SplitString(strTypes,_T(':'),strlst);
-    if(strlst.GetCount()!=2)
-    {
-        SMessageBox(m_pPageRoot->GetContainer()->GetHostHwnd(),_T("没有指定文件类型"),_T("错误"),MB_OK|MB_ICONSTOP);
-        return;
-    }
-    SStringTList lstTypes;
-    SplitString(strlst[1],_T(';'),lstTypes);
-    CODECFGMAP cfg;
-    for(int i=0;i<lstTypes.GetCount();i++)
-    {
-        cfg[lstTypes[i]] = m_mapCodeCfg[lstTypes[i]];
-    }
-
-    //获得文件列表，计每个目标文件的文件大小
-    HSTREEITEM hRoot = m_pDirTree->GetRootItem();
-    SList<FILEINFO> lstFileInfo;
-    int nDirs = 1;
-    DWORD szAll = EnumFileInfo(cfg,m_strDir,hRoot,lstFileInfo,nDirs);
-    if(lstFileInfo.GetCount() == 0)
-    {
-        SMessageBox(m_pPageRoot->GetContainer()->GetHostHwnd(),_T("指定的目录下没有找到满足条件的文件类型"),_T("提示"),MB_OK|MB_ICONINFORMATION);
-        return;
-    }
-
-    SWindow *pPageResult = m_pPageRoot->FindChildByName(L"page_dir");   
-    SSplitWnd_Row *pSplitTree = m_pPageRoot->FindChildByName2<SSplitWnd_Row>(L"split_tree");
-    pSplitTree->ShowPane(1);
-
-    SProgress *pProgBar = pSplitTree->FindChildByName2<SProgress>(L"prog_run");
-    pProgBar->SetRange(0,szAll);
-    pProgBar->SetValue(0);
-
-    int nCodeLinesAll=0,nBlankLinesAll=0,nRemLinesAll=0;
-
-    //统计代码行
-    DWORD dwProg = 0;
-    SPOSITION pos = lstFileInfo.GetHeadPosition();
-    while(pos)
-    {
-        FILEINFO fi = lstFileInfo.GetNext(pos);
-        int nCodeLines=0,nBlankLines=0,nRemLines=0;
-        CountCodeLines(S_CW2T(fi.strFileName),fi.cfg,nCodeLines,nRemLines,nBlankLines);
-        nCodeLinesAll += nCodeLines;
-        nBlankLinesAll += nBlankLines;
-        nRemLinesAll += nRemLines;
-
-        dwProg += fi.dwSize;
-        pProgBar->SetValue(dwProg);
-        pProgBar->UpdateWindow();
-    }
-
-    pSplitTree->HidePane(1);
-    STabCtrl *pTab = m_pPageRoot->FindChildByName2<STabCtrl>(L"tab_codeline");
-    pTab->SetCurSel(1);
-    SWindow *pWndRes = pTab->GetPage(_T("page_result"),FALSE);
-    pWndRes->FindChildByName(L"txt_folders")->SetWindowText(SStringT().Format(_T("%d"),nDirs));
-    pWndRes->FindChildByName(L"txt_files")->SetWindowText(SStringT().Format(_T("%d"),lstFileInfo.GetCount()));
-    pWndRes->FindChildByName(L"txt_codelines")->SetWindowText(SStringT().Format(_T("%d"),nCodeLinesAll));
-    pWndRes->FindChildByName(L"txt_blanklines")->SetWindowText(SStringT().Format(_T("%d"),nBlankLinesAll));
-    pWndRes->FindChildByName(L"txt_remarklines")->SetWindowText(SStringT().Format(_T("%d"),nRemLinesAll));
-
-}
-
-
 DWORD CCodeLineCounterHandler::EnumFileInfo(const CODECFGMAP &cfg, const SStringW & strPath, HSTREEITEM hItem,SList<FILEINFO> & lstFileInfo , int &nDirs)
 {
     DWORD fileSizeDir = 0;
@@ -189,7 +221,7 @@ DWORD CCodeLineCounterHandler::EnumFileInfo(const CODECFGMAP &cfg, const SString
                 LPCWSTR pszExt = wcsrchr(fd.cFileName,L'.');
                 if(!pszExt) continue;
                 pszExt ++;
-                
+
                 const CODECFGMAP::CPair * pPair = cfg.Lookup(pszExt);
                 if(!pPair) continue;
                 FILEINFO fi;
@@ -219,3 +251,96 @@ DWORD CCodeLineCounterHandler::EnumFileInfo(const CODECFGMAP &cfg, const SString
     }
     return fileSizeDir;
 }
+
+void CCodeLineCounterHandler::OnBtnGo()
+{
+    if(m_strDir.IsEmpty())
+    {
+        SMessageBox(m_pPageRoot->GetContainer()->GetHostHwnd(),_T("没有指定扫描目录"),_T("错误"),MB_OK|MB_ICONSTOP);
+        return;
+    }
+    SStringT strTypes = m_pPageRoot->FindChildByName(L"cbx_filetypes")->GetWindowText();
+    SStringTList strlst;
+    SplitString(strTypes,_T(':'),strlst);
+    if(strlst.GetCount()!=2)
+    {
+        SMessageBox(m_pPageRoot->GetContainer()->GetHostHwnd(),_T("没有指定文件类型"),_T("错误"),MB_OK|MB_ICONSTOP);
+        return;
+    }
+    SStringWList lstTypes;
+    SplitString(S_CT2W(strlst[1]),L';',lstTypes);
+    CODECFGMAP cfg;
+    for(int i=0;i<lstTypes.GetCount();i++)
+    {
+        cfg[lstTypes[i]] = m_mapCodeCfg[lstTypes[i]];
+    }
+
+    //获得文件列表，计每个目标文件的文件大小
+    HSTREEITEM hRoot = m_pDirTree->GetRootItem();
+    SList<FILEINFO> lstFileInfo;
+    int nDirs = 1;
+    DWORD szAll = EnumFileInfo(cfg,m_strDir,hRoot,lstFileInfo,nDirs);
+    if(lstFileInfo.GetCount() == 0)
+    {
+        SMessageBox(m_pPageRoot->GetContainer()->GetHostHwnd(),_T("指定的目录下没有找到满足条件的文件类型"),_T("提示"),MB_OK|MB_ICONINFORMATION);
+        return;
+    }
+
+    SWindow *pPageResult = m_pPageRoot->FindChildByName(L"page_dir");   
+    SSplitWnd_Row *pSplitTree = m_pPageRoot->FindChildByName2<SSplitWnd_Row>(L"split_tree");
+    pSplitTree->ShowPane(1);
+
+    SProgress *pProgBar = pSplitTree->FindChildByName2<SProgress>(L"prog_run");
+    pProgBar->SetRange(0,szAll);
+    pProgBar->SetValue(0);
+
+    CLineInfo lineInfoAll;
+
+    SMap<SStringT,CLineInfo> mapLineInfo;
+    for(int i=0;i<lstTypes.GetCount();i++)
+    {
+        mapLineInfo[lstTypes[i]] = CLineInfo();
+    }
+
+    //统计代码行
+    DWORD dwProg = 0;
+    SPOSITION pos = lstFileInfo.GetHeadPosition();
+    while(pos)
+    {
+        FILEINFO fi = lstFileInfo.GetNext(pos);
+
+        CLineInfo lineInfo;
+        CountCodeLines(S_CW2T(fi.strFileName),fi.cfg,lineInfo.nCodeLines,lineInfo.nRemarkLines,lineInfo.nBlankLines);
+        lineInfoAll += lineInfo;
+        mapLineInfo[fi.cfg.strExt] += lineInfo;
+
+        dwProg += fi.dwSize;
+        pProgBar->SetValue(dwProg);
+        pProgBar->UpdateWindow();
+    }
+
+    pSplitTree->HidePane(1);
+    STabCtrl *pTab = m_pPageRoot->FindChildByName2<STabCtrl>(L"tab_codeline");
+    pTab->SetCurSel(1);
+    SWindow *pWndRes = pTab->GetPage(_T("page_result"),FALSE);
+    pWndRes->FindChildByName(L"txt_folders")->SetWindowText(SStringT().Format(_T("%d"),nDirs));
+    pWndRes->FindChildByName(L"txt_files")->SetWindowText(SStringT().Format(_T("%d"),lstFileInfo.GetCount()));
+    pWndRes->FindChildByName(L"txt_codelines")->SetWindowText(SStringT().Format(_T("%d"),lineInfoAll.nCodeLines));
+    pWndRes->FindChildByName(L"txt_blanklines")->SetWindowText(SStringT().Format(_T("%d"),lineInfoAll.nBlankLines));
+    pWndRes->FindChildByName(L"txt_remarklines")->SetWindowText(SStringT().Format(_T("%d"),lineInfoAll.nRemarkLines));
+
+    SListCtrl *plstReport = pWndRes->FindChildByName2<SListCtrl>(L"lst_linecounter_report");
+    plstReport->DeleteAllItems();
+    for(int i=0;i<lstTypes.GetCount();i++)
+    {
+        CCodeConfig codeCfg = cfg[lstTypes[i]];
+        CLineInfo lineInfo = mapLineInfo[lstTypes[i]];
+
+        plstReport->InsertItem(i,codeCfg.strType);
+        plstReport->SetSubItemText(i,1,codeCfg.strExt);
+        plstReport->SetSubItemText(i,2,SStringT().Format(_T("%d"),lineInfo.nCodeLines));
+        plstReport->SetSubItemText(i,3,SStringT().Format(_T("%d"),lineInfo.nBlankLines));
+        plstReport->SetSubItemText(i,4,SStringT().Format(_T("%d"),lineInfo.nRemarkLines));
+    }
+}
+
