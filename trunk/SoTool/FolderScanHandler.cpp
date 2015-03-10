570dbdfc0524a4f3ab22dd8ada5f837dcbc146a2
setoutsoft <setoutsoft@5312f6f6-3d4f-41ab-b741-a9c9e676edf4>
2015-03-10 09:23:01 +0000

    增加treectrl的子节点排序功能，测试还没有通过。

--- trunk/SoTool/FolderScanHandler.cpp
+++ trunk/SoTool/FolderScanHandler.cpp
@@ -80,9 +80,59 @@ void CFolderScanHandler::OnInit(SWindow *pRoot)
     SASSERT(m_pTreelist);
 }
 
+BOOL CFolderScanHandler::DoSomething()
+{
+    MSG msg;
+    while(PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+    {
+        if(msg.message == PM_REMOVE)
+        {
+            PostThreadMessage(0,WM_QUIT,0,0);
+            return FALSE;
+        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return TRUE;
+}
+
+BOOL CFolderScanHandler::EnumFiles(SStringT strPath,HSTREEITEM hParent)
+{
+    BOOL bRet = TRUE;
+    WIN32_FIND_DATA fd;
+    SStringT strFind = strPath + _T("\\*.*");
+    HANDLE hFind=FindFirstFile(strFind,&fd);
+    if(hFind != INVALID_HANDLE_VALUE)
+    {
+        while(bRet && FindNextFile(hFind,&fd))
+        {
+            SStringT strName= fd.cFileName;
+            BOOL bFolder = fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
+            if(bFolder && (strName == _T(".") || strName == _T("..")))
+                continue;
+            __int64 nSize = ((__int64)fd.nFileSizeHigh)<<32|fd.nFileSizeLow;
+            HSTREEITEM hItem = m_pTreelist->GetFolderTreeCtrl()->InsertItem(strName,bFolder,nSize,hParent);
+            if(!DoSomething())
+            {
+                bRet = FALSE;
+            }else if(bFolder)
+            {
+                bRet = EnumFiles(strPath + _T("\\") + strName,hItem);
+            }
+        }
+        FindClose(hFind);
+    }
+    return bRet;
+}
+
+
 void CFolderScanHandler::OnGo(EventArgs *pEvt)
 {
+    SWindow *pBtn = m_pPageRoot->FindChildByName(L"btn_go");
     SWindow *pEditDir = m_pPageRoot->FindChildByName(L"edit_dir");
+    pBtn->EnableWindow(FALSE,TRUE);
+    pEditDir->EnableWindow(FALSE,TRUE);
+    
     SStringT strDir = pEditDir->GetWindowText();
     DWORD dwAttr = GetFileAttributes(strDir);
     if(dwAttr ==INVALID_FILE_ATTRIBUTES)
@@ -90,7 +140,19 @@ void CFolderScanHandler::OnGo(EventArgs *pEvt)
     if(!(dwAttr & FILE_ATTRIBUTE_DIRECTORY))
         return;
     
+    SWindow *pScanAni = m_pPageRoot->FindChildByName(L"ani_scan");
+    pScanAni->SetVisible(TRUE,TRUE);
     SFolderTreeCtrl *pMcTreeCtrl = m_pTreelist->GetFolderTreeCtrl();
     pMcTreeCtrl->RemoveAllItems();
-    HSTREEITEM hRoot = pMcTreeCtrl->InsertItem(_T("root"),TRUE,12345,STVI_ROOT);
+    HSTREEITEM hRoot = pMcTreeCtrl->InsertItem(_T("root"),TRUE,0,STVI_ROOT);
+    if(EnumFiles(strDir,hRoot))
+    {
+        m_pTreelist->GetFolderTreeCtrl()->GetFileInfo(hRoot)->percent=100;
+        m_pTreelist->GetFolderTreeCtrl()->UpdateTreeItemPercent(hRoot);
+    }
+    m_pTreelist->GetFolderTreeCtrl()->Invalidate();
+    pBtn->EnableWindow(TRUE,TRUE);
+    pEditDir->EnableWindow(TRUE,TRUE);
+    pScanAni->SetVisible(FALSE,TRUE);
+
 }
--- trunk/SoTool/FolderScanHandler.h
+++ trunk/SoTool/FolderScanHandler.h
@@ -10,13 +10,15 @@ public:
     void OnInit(SWindow *pRoot);
 protected:
     void OnGo(EventArgs *pEvt);
+    BOOL EnumFiles(SStringT strPath,HSTREEITEM hParent);
+    BOOL DoSomething();
     
     EVENT_MAP_BEGIN()
         EVENT_CHECK_SENDER_ROOT(m_pPageRoot)
         EVENT_NAME_HANDLER(L"btn_go",EventCmd::EventID,OnGo)
         EVENT_NAME_HANDLER(L"edit_dir",EventKeyEnter::EventID,OnGo)
     EVENT_MAP_BREAK()
-    
+
     SWindow *m_pPageRoot;
     SFolderTreeList *m_pTreelist;
 };
