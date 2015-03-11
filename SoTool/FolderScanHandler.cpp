a202be575bb656f0b21a7255aec64f03e2582483
setoutsoft <setoutsoft@users.noreply.github.com>
2015-03-11 14:54:27 +0000

    调整sotool

--- SoTool/FolderScanHandler.cpp
+++ SoTool/FolderScanHandler.cpp
@@ -1,61 +1,5 @@
 #include "stdafx.h"
 #include "FolderScanHandler.h"
-#include "droptarget.h"
-
-//////////////////////////////////////////////////////////////////////////
-class CDropTarget_Dir2 : public CDropTarget
-{
-protected:
-    SWindow *m_pEdit;
-public:
-    CDropTarget_Dir2(SWindow *pEdit):m_pEdit(pEdit)
-    {
-    }
-    ~CDropTarget_Dir2()
-    {
-    }
-public:
-    virtual HRESULT STDMETHODCALLTYPE Drop( 
-        /* [unique][in] */ __RPC__in_opt IDataObject *pDataObj,
-        /* [in] */ DWORD grfKeyState,
-        /* [in] */ POINTL pt,
-        /* [out][in] */ __RPC__inout DWORD *pdwEffect)
-    {
-        FORMATETC format =
-        {
-            CF_HDROP, 0, DVASPECT_CONTENT, -1, TYMED_HGLOBAL
-        };
-        STGMEDIUM medium;
-        if(FAILED(pDataObj->GetData(&format, &medium)))
-        {
-            return S_FALSE;
-        }
-
-        HDROP hdrop = static_cast<HDROP>(GlobalLock(medium.hGlobal));
-
-        if(!hdrop)
-        {
-            return S_FALSE;
-        }
-
-        bool success = false;
-        TCHAR filename[MAX_PATH];
-        success=!!DragQueryFile(hdrop, 0, filename, MAX_PATH);
-        if(success) 
-        {
-            if(GetFileAttributes(filename) & FILE_ATTRIBUTE_DIRECTORY)
-            {
-                m_pEdit->SetWindowText(filename);
-            }
-        }
-        DragFinish(hdrop);
-        GlobalUnlock(medium.hGlobal);
-
-
-        *pdwEffect=DROPEFFECT_LINK;
-        return S_OK;
-    }
-};
 
 //////////////////////////////////////////////////////////////////////////
 CFolderScanHandler::CFolderScanHandler(void):m_pPageRoot(NULL)
@@ -66,13 +10,27 @@ CFolderScanHandler::~CFolderScanHandler(void)
 {
 }
 
+void CFolderScanHandler::OnFileDropdown( HDROP hDrop )
+{
+    bool success = false;
+    TCHAR filename[MAX_PATH];
+    success=!!DragQueryFile(hDrop, 0, filename, MAX_PATH);
+    if(success) 
+    {
+        if(GetFileAttributes(filename) & FILE_ATTRIBUTE_DIRECTORY)
+        {
+            InitDir(filename);
+        }
+    }
+}
+
 void CFolderScanHandler::OnInit(SWindow *pRoot)
 {
     m_pPageRoot = pRoot->FindChildByName(L"page_folderscan");
     SASSERT(m_pPageRoot);
     SWindow *pEditDir = m_pPageRoot->FindChildByName(L"edit_dir");
     SASSERT(pEditDir);
-    IDropTarget *pDT = new CDropTarget_Dir2(pEditDir);
+    IDropTarget *pDT = new CDropTarget(this);
     m_pPageRoot->GetContainer()->RegisterDragDrop(pEditDir->GetSwnd(),pDT);
     pDT->Release();
 
@@ -157,18 +115,26 @@ BOOL CFolderScanHandler::EnumFiles(SStringT strPath,HSTREEITEM hParent)
 
 void CFolderScanHandler::OnGo(EventArgs *pEvt)
 {
+    SWindow *pEditDir = m_pPageRoot->FindChildByName(L"edit_dir");
+    InitDir(pEditDir->GetWindowText());
+}
+
+void CFolderScanHandler::InitDir( const SStringT & strDir )
+{
     SWindow *pBtn = m_pPageRoot->FindChildByName(L"btn_go");
     SWindow *pEditDir = m_pPageRoot->FindChildByName(L"edit_dir");
-    pBtn->EnableWindow(FALSE,TRUE);
-    pEditDir->EnableWindow(FALSE,TRUE);
-    
-    SStringT strDir = pEditDir->GetWindowText();
+
     DWORD dwAttr = GetFileAttributes(strDir);
     if(dwAttr ==INVALID_FILE_ATTRIBUTES)
         return;
     if(!(dwAttr & FILE_ATTRIBUTE_DIRECTORY))
         return;
-    
+
+    pEditDir->SetWindowText(strDir);
+
+    pBtn->EnableWindow(FALSE,TRUE);
+    pEditDir->EnableWindow(FALSE,TRUE);
+
     SWindow *pScanAni = m_pPageRoot->FindChildByName(L"ani_scan");
     pScanAni->SetVisible(TRUE,TRUE);
     SFolderTreeCtrl *pMcTreeCtrl = m_pTreelist->GetFolderTreeCtrl();
@@ -183,5 +149,4 @@ void CFolderScanHandler::OnGo(EventArgs *pEvt)
     pBtn->EnableWindow(TRUE,TRUE);
     pEditDir->EnableWindow(TRUE,TRUE);
     pScanAni->SetVisible(FALSE,TRUE);
-
 }
