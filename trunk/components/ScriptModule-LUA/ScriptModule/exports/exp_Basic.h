4e241193e90cf60dc2c3255ea3e5b39be46edc99
setoutsoft <setoutsoft@5312f6f6-3d4f-41ab-b741-a9c9e676edf4>
2015-03-15 13:08:18 +0000

    升级lua内核到5.2.3

--- trunk/components/ScriptModule-LUA/ScriptModule/exports/exp_Basic.h
+++ trunk/components/ScriptModule-LUA/ScriptModule/exports/exp_Basic.h
@@ -34,16 +34,10 @@ BOOL ExpLua_Basic(lua_State *L)
 		lua_tinker::class_add<CPoint>(L,"CPoint");
 		lua_tinker::class_inh<CPoint,POINT>(L);
 		lua_tinker::class_con<CPoint>(L,lua_tinker::constructor<CPoint,LONG,LONG>);
-		lua_tinker::class_mem<CPoint>(L, "x", &CPoint::x);
-		lua_tinker::class_mem<CPoint>(L, "y", &CPoint::y);
 		//CRect
 		lua_tinker::class_add<CRect>(L,"CRect");
 		lua_tinker::class_inh<CRect,RECT>(L);
 		lua_tinker::class_con<CRect>(L,lua_tinker::constructor<CRect,LONG,LONG,LONG,LONG>);
-		lua_tinker::class_mem<CRect>(L, "left", &CRect::left);
-		lua_tinker::class_mem<CRect>(L, "top", &CRect::top);
-		lua_tinker::class_mem<CRect>(L, "right", &CRect::right);
-		lua_tinker::class_mem<CRect>(L, "bottom", &CRect::bottom);
 		lua_tinker::class_def<CRect>(L,"Width",&CRect::Width);
 		lua_tinker::class_def<CRect>(L,"Height",&CRect::Height);
 		lua_tinker::class_def<CRect>(L,"Size",&CRect::Size);
@@ -58,8 +52,6 @@ BOOL ExpLua_Basic(lua_State *L)
 		lua_tinker::class_add<CSize>(L,"CSize");
 		lua_tinker::class_inh<CSize,SIZE>(L);
 		lua_tinker::class_con<CSize>(L,lua_tinker::constructor<CSize,LONG,LONG>);
-		lua_tinker::class_mem<CSize>(L, "cx", &CSize::cx);
-		lua_tinker::class_mem<CSize>(L, "cy", &CSize::cy);
 
 		return TRUE;
 	}catch(...)
--- trunk/components/ScriptModule-LUA/ScriptModule/require.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define DLL_CORE
-typedef char * _cstring;
-typedef void * _userdata;
-#define __op_run	operator()
