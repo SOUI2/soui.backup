ad0bee2edea512b5543d21d74983665912aac178
setoutsoft <setoutsoft@users.noreply.github.com>
2016-05-02 12:49:39 +0000

    从CXImage里移植着色算法。

--- SOUI/src/helper/SDIBHelper.cpp
+++ SOUI/src/helper/SDIBHelper.cpp
@@ -1,10 +1,10 @@
 #include "souistd.h"
 #include "helper/SDIBHelper.h"
 
+#define RGB2GRAY(r,g,b) (((b)*117 + (g)*601 + (r)*306) >> 10)
+
 namespace SOUI
 {
-    enum{Red,Green,Blue};
-
     struct DIBINFO{
         LPBYTE  pBits;
         int     nWid;
@@ -27,232 +27,238 @@ namespace SOUI
         LPBYTE pBit = pDib->pBits;
         for(int i=0;i<nPixels;i++, pBit+=4)
         {
-            mode(pBit[2], pBit[1], pBit[0], param);
+            mode(pBit, param);
         }
 
 		return true;
 	}
 
+
 	// 灰度 = 0.299 * red + 0.587 * green + 0.114 * blue 
-	inline void MaptoGray(BYTE & red, BYTE & green, BYTE & blue, const int &)
+	inline void GrayMode(BYTE *pColor,const int &)
 	{
-		red   = (red * 77 + green * 150 + blue * 29 + 128) / 256;
-		green = red;
-		blue  = red;
+        pColor[0] = pColor[1] = pColor[2] = RGB2GRAY(pColor[0],pColor[1],pColor[2]);
 	}
-
-    struct HSL32PARAM
-    {
-        int degHue;
-        int perSaturation;
-        int perLuminosity;
+    
+    struct COLORIZEPARAM{
+        BYTE hue;
+        BYTE sat;
+        int  a0;//[0-256]
+        int  a1;//[0-256]
     };
-    
-    inline void AdjustPixelHSL(BYTE & red, BYTE & green, BYTE & blue,const HSL32PARAM & param)
+
+    void FillColorizeParam(COLORIZEPARAM &param ,BYTE hue,BYTE sat,float fBlend)
     {
-        float H = 0.0f;
-        float S = 0.0f;
-        float L = 0.0f;	
-
-        SDIBHelper::RGBtoHSL(red,green,blue,
-            H, S, L);
-
-        H += param.degHue;
-        S = (S*(float)param.perSaturation/100.0f);
-        L = (L*(float)param.perLuminosity/100.0f);
-
-        SDIBHelper::HSLtoRGB(H, S, L, red,green,blue);
+        param.hue = hue;
+        param.sat = sat;
+        SASSERT(fBlend>=0.0f && fBlend <=1.0f);
+        param.a0 = (int)(fBlend*256);
+        param.a1 = 256 - param.a0;
     }
-    
-	bool SDIBHelper::AdjustHSL32(IBitmap * pBmp,int degHue,int perSaturation,int perLuminosity)
-	{
+
+    ////////////////////////////////////////////////////////////////////////////////
+    #define  HSLMAX   255	/* H,L, and S vary over 0-HSLMAX */
+    #define  RGBMAX   255   /* R,G, and B vary over 0-RGBMAX */
+        /* HSLMAX BEST IF DIVISIBLE BY 6 */
+        /* RGBMAX, HSLMAX must each fit in a BYTE. */
+        /* Hue is undefined if Saturation is 0 (grey-scale) */
+        /* This value determines where the Hue scrollbar is */
+        /* initially set for achromatic colors */
+    #define HSLUNDEFINED (HSLMAX*2/3)
+    ////////////////////////////////////////////////////////////////////////////////
+    RGBQUAD RGBtoHSL(RGBQUAD lRGBColor)
+    {
+        BYTE R,G,B;					/* input RGB values */
+        BYTE H,L,S;					/* output HSL values */
+        BYTE cMax,cMin;				/* max and min RGB values */
+        WORD Rdelta,Gdelta,Bdelta;	/* intermediate value: % of spread from max*/
+
+        R = lRGBColor.rgbRed;	/* get R, G, and B out of DWORD */
+        G = lRGBColor.rgbGreen;
+        B = lRGBColor.rgbBlue;
+
+        cMax = max( max(R,G), B);	/* calculate lightness */
+        cMin = min( min(R,G), B);
+        L = (BYTE)((((cMax+cMin)*HSLMAX)+RGBMAX)/(2*RGBMAX));
+
+        if (cMax==cMin){			/* r=g=b --> achromatic case */
+            S = 0;					/* saturation */
+            H = HSLUNDEFINED;		/* hue */
+        } else {					/* chromatic case */
+            if (L <= (HSLMAX/2))	/* saturation */
+                S = (BYTE)((((cMax-cMin)*HSLMAX)+((cMax+cMin)/2))/(cMax+cMin));
+            else
+                S = (BYTE)((((cMax-cMin)*HSLMAX)+((2*RGBMAX-cMax-cMin)/2))/(2*RGBMAX-cMax-cMin));
+            /* hue */
+            Rdelta = (WORD)((((cMax-R)*(HSLMAX/6)) + ((cMax-cMin)/2) ) / (cMax-cMin));
+            Gdelta = (WORD)((((cMax-G)*(HSLMAX/6)) + ((cMax-cMin)/2) ) / (cMax-cMin));
+            Bdelta = (WORD)((((cMax-B)*(HSLMAX/6)) + ((cMax-cMin)/2) ) / (cMax-cMin));
+
+            if (R == cMax)
+                H = (BYTE)(Bdelta - Gdelta);
+            else if (G == cMax)
+                H = (BYTE)((HSLMAX/3) + Rdelta - Bdelta);
+            else /* B == cMax */
+                H = (BYTE)(((2*HSLMAX)/3) + Gdelta - Rdelta);
+
+            //		if (H < 0) H += HSLMAX;     //always false
+            if (H > HSLMAX) H -= HSLMAX;
+        }
+        RGBQUAD hsl={L,S,H,0};
+        return hsl;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    RGBQUAD RGBtoRGBQUAD(COLORREF cr)
+    {
+        RGBQUAD c;
+        c.rgbRed = GetRValue(cr);	/* get R, G, and B out of DWORD */
+        c.rgbGreen = GetGValue(cr);
+        c.rgbBlue = GetBValue(cr);
+        c.rgbReserved=0;
+        return c;
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    COLORREF RGBQUADtoRGB (RGBQUAD c)
+    {
+        return RGB(c.rgbRed,c.rgbGreen,c.rgbBlue);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    float HueToRGB(float n1,float n2, float hue)
+    {
+        //<F. Livraghi> fixed implementation for HSL2RGB routine
+        float rValue;
+
+        if (hue > 360)
+            hue = hue - 360;
+        else if (hue < 0)
+            hue = hue + 360;
+
+        if (hue < 60)
+            rValue = n1 + (n2-n1)*hue/60.0f;
+        else if (hue < 180)
+            rValue = n2;
+        else if (hue < 240)
+            rValue = n1+(n2-n1)*(240-hue)/60;
+        else
+            rValue = n1;
+
+        return rValue;
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    RGBQUAD HSLtoRGB(RGBQUAD lHSLColor)
+    { 
+        //<F. Livraghi> fixed implementation for HSL2RGB routine
+        float h,s,l;
+        float m1,m2;
+        BYTE r,g,b;
+
+        h = (float)lHSLColor.rgbRed * 360.0f/255.0f;
+        s = (float)lHSLColor.rgbGreen/255.0f;
+        l = (float)lHSLColor.rgbBlue/255.0f;
+
+        if (l <= 0.5)	m2 = l * (1+s);
+        else			m2 = l + s - l*s;
+
+        m1 = 2 * l - m2;
+
+        if (s == 0) {
+            r=g=b=(BYTE)(l*255.0f);
+        } else {
+            r = (BYTE)(HueToRGB(m1,m2,h+120) * 255.0f);
+            g = (BYTE)(HueToRGB(m1,m2,h) * 255.0f);
+            b = (BYTE)(HueToRGB(m1,m2,h-120) * 255.0f);
+        }
+
+        RGBQUAD rgb = {b,g,r,0};
+        return rgb;
+    }
+
+    inline void ColorizeMode(BYTE *pAbgr, const COLORIZEPARAM & param)
+    {
+        BYTE red = pAbgr[0],green=pAbgr[1],blue=pAbgr[2],alpha=pAbgr[3];
+        
+        if(alpha == 0) return;
+        if(alpha!=255)
+        {
+            red = (red * 255)/alpha;
+            green = (green * 255)/alpha;
+            blue = (blue * 255)/alpha;
+        }
+        RGBQUAD pixel = {blue,green, red};
+
+        if (param.a0 == 256){
+            RGBQUAD color = RGBtoHSL(pixel);
+            color.rgbRed= param.hue;
+            color.rgbGreen=param.sat;
+            pixel = HSLtoRGB(color);
+            red = pixel.rgbRed;
+            green = pixel.rgbGreen;
+            blue = pixel.rgbBlue;
+        } else {
+            RGBQUAD color = pixel;
+            RGBQUAD hsl;
+            hsl.rgbRed= param.hue;
+            hsl.rgbGreen=param.sat;
+            hsl.rgbBlue = (BYTE)RGB2GRAY(color.rgbRed,color.rgbGreen,color.rgbBlue);
+            hsl = HSLtoRGB(hsl);
+            red = (BYTE)((hsl.rgbRed * param.a0 + color.rgbRed * param.a1)>>8);
+            green = (BYTE)((hsl.rgbBlue * param.a0 + color.rgbBlue * param.a1)>>8);
+            blue = (BYTE)((hsl.rgbGreen * param.a0 + color.rgbGreen * param.a1)>>8);
+        }
+        if(alpha!=255)
+        {
+            red = (red *alpha)/255;
+            green = (green *alpha)/255;
+            blue = (blue *alpha)/255;
+        }
+        pAbgr[0] = red;
+        pAbgr[1] = green;
+        pAbgr[2] = blue;
+    }
+
+    bool SDIBHelper::Colorize(IBitmap * pBmp, COLORREF crRef)
+    {
+        RGBQUAD color = RGBtoRGBQUAD(crRef);
+        RGBQUAD hsl = SOUI::RGBtoHSL(color);
+        COLORIZEPARAM param;
+        FillColorizeParam(param,hsl.rgbRed,hsl.rgbGreen,0.8f);
+
         DIBINFO di={(LPBYTE)pBmp->LockPixelBits(),pBmp->Width(),pBmp->Height()};
 
-		bool bRet = false;
-		do 
-		{
-			if (perSaturation < 0||perLuminosity < 0)
-			{
-				break;
-			}
+        bool bRet = false;
+        do 
+        {
 
-			if (degHue == 0 && perSaturation == 100 && perLuminosity == 100)
-			{
-				bRet = true;// 未作调整，直接返回
-				break;
-			}
-            
-            HSL32PARAM param={degHue,perSaturation,perLuminosity};
-            bRet =ColorTransform(&di, AdjustPixelHSL,param);
-            
-		} while (false);
-		
-		pBmp->UnlockPixelBits(di.pBits);
-		return bRet;
-	}
+            bRet =ColorTransform(&di, ColorizeMode,param);
 
-    bool SDIBHelper::AdjustHue(IBitmap *pBmp,COLORREF crRef)
-    {
-        float h,s,l;
-        BYTE r=GetRValue(crRef),g=GetGValue(crRef),b=GetBValue(crRef);
-        RGBtoHSL(r,g,b,h,s,l);
-        return AdjustHSL32(pBmp,(int)h,100,100);
+        } while (false);
+
+        pBmp->UnlockPixelBits(di.pBits);
+        return bRet;
     }
 
-    bool SDIBHelper::AdjustHue(COLORREF & crTarget,COLORREF crRef)
+    bool SDIBHelper::Colorize(COLORREF & crTarget,COLORREF crRef)
     {
-        float h,s,l;
-        BYTE r=GetRValue(crRef),g=GetGValue(crRef),b=GetBValue(crRef);
-        RGBtoHSL(r,g,b,h,s,l);
-        if(h == 0) return true;// 未作调整，直接返回
-
-        HSL32PARAM param={(int)h,100,100};
-
-        r=GetRValue(crTarget),g=GetGValue(crTarget),b=GetBValue(crTarget);
-
-        AdjustPixelHSL(r,g,b,param);
-        crTarget = RGBA(r,g,b,GetAValue(crTarget));
-
+        RGBQUAD color = RGBtoRGBQUAD(crRef);
+        RGBQUAD hsl = SOUI::RGBtoHSL(color);
+        COLORIZEPARAM param;
+        FillColorizeParam(param,hsl.rgbRed,hsl.rgbGreen,0.8f);
+        
+        ColorizeMode((BYTE*)&crTarget,param);
         return true;
     }   
 
 	bool SDIBHelper::GrayImage(IBitmap * pBmp)
 	{
 	    DIBINFO di={(LPBYTE)pBmp->LockPixelBits(),pBmp->Width(),pBmp->Height()};
-		bool bRet =ColorTransform(&di, MaptoGray,0);
+		bool bRet =ColorTransform(&di, GrayMode,0);
 		pBmp->UnlockPixelBits(di.pBits);
 		return bRet;
 	}
 	
-
-	void SDIBHelper::RGBtoHSL(const BYTE &red, const BYTE &green, const BYTE &blue,
-		                      float &hue, float &saturation, float &lightness)
-	{
-		float mn  = 0.0f;
-		float mx  = 0.0f; 
-		int	major = Red;
-
-		if (red < green)
-		{
-			mn = red; 
-			mx = green;
-			major = Green;
-		}
-		else
-		{
-			mn = green;
-			mx = red; 
-			major = Red;
-		}
-
-		if (blue < mn)
-		{
-			mn = blue;
-		}
-		else if (blue > mx)
-		{
-			mx = blue;
-			major = Blue;
-		}
-
-		if (mn == mx) 
-		{
-			lightness    = mn/255.0f;
-			saturation   = 0;
-			hue          = 0; 
-		}   
-		else 
-		{ 
-			lightness = (mn+mx) / 510.0f;
-
-			if (lightness <= 0.5f)
-			{
-				saturation = (mx-mn) / (mn+mx); 
-			}
-			else
-			{
-				saturation = (mx-mn) / (510.0f-mn-mx);
-			}
-
-			switch (major)
-			{
-			case Red:
-				hue = (green-blue) * 60 / (mx-mn) + 360; 
-				break;
-
-			case Green: 
-				hue = (blue-red) * 60  / (mx-mn) + 120;  
-				break;
-
-			case Blue : hue = (red-green) * 60 / (mx-mn) + 240;
-				break;
-			}
-
-			if (hue >= 360.0f)
-			{
-				hue = hue - 360.0f;
-			}
-		}
-	}
-
-	unsigned char Value(float m1, float m2, float h)
-	{
-		while (h >= 360.0f) 
-		{
-			h -= 360.0f;
-		}
-		while (h < 0) 
-		{
-			h += 360.0f;
-		}
-
-		if (h < 60.0f) 
-		{
-			m1 = m1 + (m2 - m1) * h / 60;   
-		}
-		else if (h < 180.0f) 
-		{
-			m1 = m2;
-		}
-		else if (h < 240.0f) 
-		{
-			m1 = m1 + (m2 - m1) * (240 - h) / 60;  
-		}
-
-		return (unsigned char)(m1 * 255);
-	}
-
-
-	void SDIBHelper::HSLtoRGB(const float &hue, const float &_saturation, const float &_lightness,
-		                      BYTE &red, BYTE &green, BYTE &blue)
-	{
-		float lightness = min(1.0f, _lightness);
-		float saturation = min(1.0f, _saturation);
-
-		if (saturation == 0)
-		{
-			red = green = blue = (unsigned char) (lightness*255);
-		}
-		else
-		{
-			float m1, m2;
-
-			if (lightness <= 0.5f)
-			{
-				m2 = lightness + lightness * saturation;  
-			}
-			else       
-			{
-				m2 = lightness + saturation - lightness * saturation;
-			}
-
-			m1 = 2 * lightness - m2;   
-
-			red   = Value(m1, m2, hue + 120);   
-			green = Value(m1, m2, hue);
-			blue  = Value(m1, m2, hue - 120);
-		}
-	}
 
     COLORREF CalcAvarageRectColor(const DIBINFO &di, RECT rc)
     {
