4bc4357bc45804a85024fd62c243d91fe3ceca2d
setoutsoft <setoutsoft@5312f6f6-3d4f-41ab-b741-a9c9e676edf4>
2016-05-02 12:49:39 +0000

    从CXImage里移植着色算法。

--- trunk/SOUI/include/helper/SDIBHelper.h
+++ trunk/SOUI/include/helper/SDIBHelper.h
@@ -10,18 +10,10 @@ namespace SOUI
 	class SOUI_EXP SDIBHelper
 	{
     public:
-		/// <summary>
-		///     H:-180~180（度）,=0不作调整, S/L:0~200(建议，最大值可>200),=100不作调整
-		/// </summary>
-		/// <remarks>
-		///     HSL变换,HSL操作建议是在非预乘像素阵列下进行，但为了更快的效率，目前采用预乘像素阵列
-		///     HSL变换应该在整个像素阵列不再变化时开始做调整.不然原始像素阵列就不再准确
-		/// </remarks>
-		static bool AdjustHSL32(IBitmap * pBmp, int H, int S, int L);
 		
-		static bool AdjustHue(IBitmap *pBmp,COLORREF crRef);
+        static bool Colorize(IBitmap * pBmp, COLORREF crRef);
 		
-        static bool AdjustHue(COLORREF & crTarget,COLORREF crRef);
+        static bool Colorize(COLORREF & crTarget,COLORREF crRef);
 
 		static bool GrayImage(IBitmap * pBmp); 
         
@@ -30,9 +22,6 @@ namespace SOUI
         //int nPercent:有效值百分比，90代表最高和最低5%的值会丢掉，不参与平均。
         //int int nBlockSize:分块大小, 每次计算一个块的颜色平均值。
         static COLORREF CalcAvarageColor(IBitmap *pBmp,int nPercent=90,int nBlockSize=5);
-	public:// 辅助
-		static void RGBtoHSL(const BYTE &R, const BYTE &G, const BYTE &B, float &H, float &S, float &L);
-		static void HSLtoRGB(const float &H, const float &S, const float &L, BYTE &R, BYTE &G, BYTE &B);
     };
 
 }//namespace SOUI
