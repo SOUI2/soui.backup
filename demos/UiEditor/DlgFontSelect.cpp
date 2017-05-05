dce17ce49e73b7afa4d35e76a2d50688b4d3be88
wjl158 <wjl158@users.noreply.github.com>
2017-05-05 15:02:41 +0000

    修正编辑器设计时控件字体显示不正常的bug

--- demos/UiEditor/DlgFontSelect.cpp
+++ demos/UiEditor/DlgFontSelect.cpp
@@ -66,14 +66,14 @@ namespace SOUI
 		if (!m_strFont.IsEmpty())
 		{
 			m_pDesignerView->UseEditorUIDef(false);
-			IFontPtr ft = SFontPool::getSingleton().GetFont(m_strFont, 0);
+			IFontPtr ft = SFontPool::getSingleton().GetFont(m_strFont, 100);
 			m_pDesignerView->UseEditorUIDef(true);
 			InitInfo(ft);
 		}
 		else
 		{
 			m_pDesignerView->UseEditorUIDef(false);
-			IFontPtr ft = SFontPool::getSingleton().GetFont(_T(""),0);
+			IFontPtr ft = SFontPool::getSingleton().GetFont(_T(""),100);
 			m_pDesignerView->UseEditorUIDef(true);
 			InitInfo(ft);
 		}
