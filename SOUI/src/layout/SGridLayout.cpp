e75a91090e02a2ef47dd225273f1d9cc2fad9584
huangjianxiong <huangjianxiong@ainemo.com>
2017-07-04 23:18:04 +0800

    在demo中增加一个使用gridLayout执行计算器布局的例子，测试还有问题。

--- SOUI/src/layout/SGridLayout.cpp
+++ SOUI/src/layout/SGridLayout.cpp
@@ -176,7 +176,7 @@ namespace SOUI
 
 
 	//////////////////////////////////////////////////////////////////////////
-	SGridLayout::SGridLayout(void):m_GravityX(gCenter),m_GravityY(gMiddle)
+	SGridLayout::SGridLayout(void):m_GravityX(gCenter),m_GravityY(gMiddle),m_nCols(-1),m_nRows(-1)
 	{
 	}
 
@@ -203,7 +203,15 @@ namespace SOUI
 	{
 		SUNUSED(nWidth);
 		SUNUSED(nHeight);
-		int cells = m_nCols*m_nRows;
+		if(m_nCols==-1 && m_nRows == -1)
+			return CSize();
+		int nChilds = pParent->GetChildrenCount();
+		int nCols = m_nCols;
+		int nRows = m_nRows;
+		if(nRows == -1) nRows = (nChilds+nCols-1)/nCols;
+		else if(nCols == -1) nCols = (nChilds+nRows-1)/nRows;
+
+		int cells = nCols*nRows;
 		CSize * pCellsSize = new CSize[cells];
 		bool  * pCellsOccupy=new bool[cells];
 
@@ -222,15 +230,15 @@ namespace SOUI
 			int colSpan = pLayoutParam->nColSpan;
 			int rowSpan = pLayoutParam->nRowSpan;
 
-			colSpan = smin(colSpan,m_nCols-iCol);
-			rowSpan = smin(rowSpan,m_nRows-iRow);
+			colSpan = smin(colSpan,nCols-iCol);
+			rowSpan = smin(rowSpan,nRows-iRow);
 			SASSERT(colSpan>=1);
 			SASSERT(rowSpan>=1);
-			SASSERT(pCellsOccupy[iRow*m_nCols+iCol]==false);//保证至少有一个空间可用
+			SASSERT(pCellsOccupy[iRow*nCols+iCol]==false);//保证至少有一个空间可用
 			//计算可占用空间
 			for(int y=0;y<rowSpan;y++) for(int x=0;x<colSpan;x++)
 			{
-				int iCell = (iRow+y)*m_nCols+iCol+x;
+				int iCell = (iRow+y)*nCols+iCol+x;
 				if(pCellsOccupy[iCell])
 				{//colSpan优先
 					rowSpan = y+1;
@@ -247,25 +255,25 @@ namespace SOUI
 			szCell.cy/=rowSpan;
 			for(int y=0;y<rowSpan;y++) for(int x=0;x<colSpan;x++)
 			{
-				int iCell = (iRow+y)*m_nCols+iCol+x;
+				int iCell = (iRow+y)*nCols+iCol+x;
 				pCellsOccupy[iCell]=true;
 				pCellsSize[iCell]=szCell;
 			}
 			
 			//计算下一个网络的排列位置(先在当前行查找，再到下面行从0开始查找)
 			bool bFind = false;
-			for(int x=iCol+1;x<m_nCols;x++)
+			for(int x=iCol+1;x<nCols;x++)
 			{
-				if(!pCellsOccupy[iRow*m_nCols+x])
+				if(!pCellsOccupy[iRow*nCols+x])
 				{
 					bFind = true;
 					iCol = x;
 					break;
 				}
 			}
-			for(int y=iRow+1;y<m_nRows && !bFind;y++) for(int x=0;x<m_nCols;x++)
+			for(int y=iRow+1;y<nRows && !bFind;y++) for(int x=0;x<nCols;x++)
 			{
-				if(!pCellsOccupy[y*m_nCols+x])
+				if(!pCellsOccupy[y*nCols+x])
 				{
 					iRow = y;
 					iCol = x;
@@ -279,23 +287,23 @@ namespace SOUI
 
 		CSize szRet;
 		//计算列宽
-		for(int x=0;x<m_nCols;x++)
+		for(int x=0;x<nCols;x++)
 		{
 			int maxWid = 0;
-			for(int y=0;y<m_nRows;y++)
+			for(int y=0;y<nRows;y++)
 			{
-				int iCell=y*m_nCols+x;
+				int iCell=y*nCols+x;
 				maxWid = smax(pCellsSize[iCell].cx,maxWid);
 			}
 			szRet.cx += maxWid;
 		}
 		//计算列高
-		for(int y=0;y<m_nRows;y++)
+		for(int y=0;y<nRows;y++)
 		{
 			int maxHei = 0;
-			for(int x=0;x<m_nCols;x++)
+			for(int x=0;x<nCols;x++)
 			{
-				int iCell=y*m_nCols+x;
+				int iCell=y*nCols+x;
 				maxHei = smax(pCellsSize[iCell].cy,maxHei);
 			}
 			szRet.cy += maxHei;
@@ -304,19 +312,29 @@ namespace SOUI
 		delete []pCellsSize;
 		delete []pCellsOccupy;
 
-		szRet.cx += m_xInterval.toPixelSize(pParent->GetScale()) * (m_nCols-1);
-		szRet.cy += m_yInterval.toPixelSize(pParent->GetScale()) * (m_nRows-1);
+		szRet.cx += m_xInterval.toPixelSize(pParent->GetScale()) * (nCols-1);
+		szRet.cy += m_yInterval.toPixelSize(pParent->GetScale()) * (nRows-1);
 		return szRet;
 	}
 
 	void SGridLayout::LayoutChildren(SWindow * pParent)
 	{
+		if(m_nCols==-1 && m_nRows == -1)
+			return ;
+
+		int nCols = m_nCols;
+		int nRows = m_nRows;
+
+		int nChilds = pParent->GetChildrenCount();
+		if(nRows == -1) nRows = (nChilds+nCols-1)/nCols;
+		else if(nCols == -1) nCols = (nChilds+nRows-1)/nRows;
+
 		CRect rcParent = pParent->GetChildrenLayoutRect();
 		int xInter = m_xInterval.toPixelSize(pParent->GetScale());
 		int yInter = m_yInterval.toPixelSize(pParent->GetScale());
 
 		//先计算出每个格子的大小,算法和MeasureChildren一样，后面再考虑如何优化
-		int cells = m_nCols*m_nRows;
+		int cells = nCols*nRows;
 		CSize * pCellsSize = new CSize[cells];
 		bool  * pCellsOccupy=new bool[cells];
 		float * pCellsColWeight = new float[cells];
@@ -334,22 +352,22 @@ namespace SOUI
 		SWindow *pCell = pParent->GetNextLayoutChild(NULL);
 		while(pCell)
 		{
-			pCellsChild[iRow*m_nCols+iCol] = pCell;
+			pCellsChild[iRow*nCols+iCol] = pCell;
 			SGridLayoutParam * pLayoutParam = pCell->GetLayoutParamT<SGridLayoutParam>();
 			SASSERT(pLayoutParam);
 			//将当前网络所占用的空间位置清0
 			int colSpan = pLayoutParam->nColSpan;
 			int rowSpan = pLayoutParam->nRowSpan;
 
-			colSpan = smin(colSpan,m_nCols-iCol);
-			rowSpan = smin(rowSpan,m_nRows-iRow);
+			colSpan = smin(colSpan,nCols-iCol);
+			rowSpan = smin(rowSpan,nRows-iRow);
 			SASSERT(colSpan>=1);
 			SASSERT(rowSpan>=1);
-			SASSERT(pCellsOccupy[iRow*m_nCols+iCol]==false);//保证至少有一个空间可用
+			SASSERT(pCellsOccupy[iRow*nCols+iCol]==false);//保证至少有一个空间可用
 			//计算可占用空间
 			for(int y=0;y<rowSpan;y++) for(int x=0;x<colSpan;x++)
 			{
-				int iCell = (iRow+y)*m_nCols+iCol+x;
+				int iCell = (iRow+y)*nCols+iCol+x;
 				if(pCellsOccupy[iCell])
 				{//colSpan优先
 					rowSpan = y+1;
@@ -369,7 +387,7 @@ namespace SOUI
 			float rowWeight = pLayoutParam->fRowWeight/rowSpan;
 			for(int y=0;y<rowSpan;y++) for(int x=0;x<colSpan;x++)
 			{
-				int iCell = (iRow+y)*m_nCols+iCol+x;
+				int iCell = (iRow+y)*nCols+iCol+x;
 				pCellsOccupy[iCell]=true;
 				pCellsSize[iCell]=szCell;
 				pCellsColWeight[iCell] = colWeight;
@@ -377,22 +395,22 @@ namespace SOUI
 				pCellsSpan[iCell].x = 0;
 				pCellsSpan[iCell].y = 0;
 			}
-			pCellsSpan[iRow*m_nCols+iCol]=CPoint(colSpan,rowSpan);
+			pCellsSpan[iRow*nCols+iCol]=CPoint(colSpan,rowSpan);
 
 			//计算下一个网络的排列位置(先在当前行查找，再到下面行从0开始查找)
 			bool bFind = false;
-			for(int x=iCol+1;x<m_nCols;x++)
+			for(int x=iCol+1;x<nCols;x++)
 			{
-				if(!pCellsOccupy[iRow*m_nCols+x])
+				if(!pCellsOccupy[iRow*nCols+x])
 				{
 					bFind = true;
 					iCol = x;
 					break;
 				}
 			}
-			for(int y=iRow+1;y<m_nRows && !bFind;y++) for(int x=0;x<m_nCols;x++)
+			for(int y=iRow+1;y<nRows && !bFind;y++) for(int x=0;x<nCols;x++)
 			{
-				if(!pCellsOccupy[y*m_nCols+x])
+				if(!pCellsOccupy[y*nCols+x])
 				{
 					iRow = y;
 					iCol = x;
@@ -405,18 +423,18 @@ namespace SOUI
 		}
 
 
-		int *pCellsWidth = new int[m_nCols];
+		int *pCellsWidth = new int[nCols];
 		int nTotalWidth=0;
-		float *pColsWeight = new float[m_nCols];
+		float *pColsWeight = new float[nCols];
 		float totalColsWeight =0.0f;
 		//计算列宽及相应的weight
-		for(int x=0;x<m_nCols;x++)
+		for(int x=0;x<nCols;x++)
 		{
 			int maxWid = 0;
 			float maxWeight = 0.0f;
-			for(int y=0;y<m_nRows;y++)
+			for(int y=0;y<nRows;y++)
 			{
-				int iCell=y*m_nCols+x;
+				int iCell=y*nCols+x;
 				maxWid = smax(pCellsSize[iCell].cx,maxWid);
 				maxWeight = smax(pCellsColWeight[iCell],maxWeight);
 			}
@@ -426,17 +444,17 @@ namespace SOUI
 			totalColsWeight += maxWeight;
 		}
 		//计算列高
-		int *pCellsHeight = new int[m_nRows];
+		int *pCellsHeight = new int[nRows];
 		int nTotalHeight =0;
-		float *pRowsWeight = new float[m_nRows];
+		float *pRowsWeight = new float[nRows];
 		float fTotalRowsWeight = 0.0f;
-		for(int y=0;y<m_nRows;y++)
+		for(int y=0;y<nRows;y++)
 		{
 			int maxHei = 0;
 			float maxWeight = 0.0f;
-			for(int x=0;x<m_nCols;x++)
+			for(int x=0;x<nCols;x++)
 			{
-				int iCell=y*m_nCols+x;
+				int iCell=y*nCols+x;
 				maxHei = smax(pCellsSize[iCell].cy,maxHei);
 				maxWeight = smax(pCellsRowWeight[iCell],maxWeight);
 			}
@@ -452,20 +470,20 @@ namespace SOUI
 		delete []pCellsRowWeight;
 
 		//分配weight
-		int netParentWid = rcParent.Width()-(m_nCols-1)*xInter;
+		int netParentWid = rcParent.Width()-(nCols-1)*xInter;
 		if(nTotalWidth<netParentWid && totalColsWeight>0.0f)
 		{
 			int nRemain = netParentWid - nTotalWidth;
-			for(int i=0;i<m_nCols;i++)
+			for(int i=0;i<nCols;i++)
 			{
 				pCellsWidth[i]+=(int)(nRemain*pColsWeight[i]/totalColsWeight);
 			}
 		}
-		int netParentHei = rcParent.Height() - (m_nRows-1)*yInter;
+		int netParentHei = rcParent.Height() - (nRows-1)*yInter;
 		if(nTotalHeight < netParentHei && fTotalRowsWeight>0.0f)
 		{
 			int nRemain = netParentHei-nTotalHeight;
-			for(int i=0;i<m_nRows;i++)
+			for(int i=0;i<nRows;i++)
 			{
 				pCellsHeight[i]+=(int)(nRemain*pRowsWeight[i]/fTotalRowsWeight);
 			}
@@ -475,11 +493,11 @@ namespace SOUI
 
 		//计算子窗口位置
 		CPoint pt = rcParent.TopLeft();
-		for(int y=0;y<m_nRows;y++)
+		for(int y=0;y<nRows;y++)
 		{
-			for(int x=0;x<m_nCols;x++)
+			for(int x=0;x<nCols;x++)
 			{
-				int iCell = y*m_nCols+x;
+				int iCell = y*nCols+x;
 				if(pCellsSpan[iCell].x==0 || pCellsSpan[iCell].y==0) continue;
 				SWindow *pCell = pCellsChild[iCell];
 				if(!pCell) break;
